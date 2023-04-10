4ad4149592895edbc2345393b5ef5bbfe51b5dc8
Kingsley Ulinfun <kingsleyulinfun@gmail.com>
2023-04-10 13:53:38 -0400

    new file:   database/__pycache__/ 	new file:   database/struct.py

---
+++ database/struct.py
@@ -0,0 +1,24 @@
+"""10/04/2023
+Kingsley U.
+CS50 AP
+Master Project- Cookbook/Recipebook"""
+
+import os,sys
+import mysql.connector
+
+class Datastructure():
+    
+    def connect(self):
+        """Creates a connection object to the database
+        """
+        
+        
+        
+    def __init__(self) -> None:
+        print(self.connect.__doc__)
+        
+def main():
+    a = Datastructure()
+    
+if (__name__ == "__main__"):
+  main()
