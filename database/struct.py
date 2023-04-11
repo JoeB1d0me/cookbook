e9e546a1ecccd4e277f2611133e0b22cd0c7f941
Kingsley Ulinfun <kingsleyulinfun@gmail.com>
2023-04-11 14:23:53 -0400

    modified:   database/struct.py

--- database/struct.py
+++ database/struct.py
@@ -8,14 +8,47 @@ import mysql.connector
 
 class Datastructure():
     
-    def connect(self):
-        """Creates a connection object to the database
+    def connect(self,host,port,user,password,database):
+        """Creates a connection object to the database"""
+        try:
+            self.conn = mysql.connector.connect(
+                host = host,
+                port = port,
+                user = user,
+                password = password,
+                database = database   
+            )
+            self.cursor = self.conn.cursor()
+            self.connected = True
+        except mysql.connector.Error:
+            print("Error with connection")
+            return 1
+        print("Connection worked")
+        return 0
+    
+    def disconnect(self):
+        """Closes the mysql connection
         """
+        if(self.connected):
+            try:
+                self.conn.close()
+                self.connected = False
+            except mysql.connector.Error:
+                print("Disconnect failed")
+                return 1
+            print("Discconnect success")
+            return 0
+        
+    def load_recipe(self):
+        pass
         
         
         
     def __init__(self) -> None:
-        print(self.connect.__doc__)
+        self.connected = False
+        self.recipe = []
+        self.ingredient_records = []
+        
         
 def main():
     a = Datastructure()
