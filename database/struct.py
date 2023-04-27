c1bf6a8615894d865959ab365fc7c97f26eb77f5
Kingsley Ulinfun <kingsleyulinfun@gmail.com>
2023-04-27 14:22:16 -0400

    modified:   database/struct.py

--- database/struct.py
+++ database/struct.py
@@ -38,142 +38,82 @@ class DataStructure():
                 return 1
             print("Discconnect success")
             return 0
-        
     def add_recipe(self, name, description, instructions, category_id):
-        query = "INSERT INTO recipes (name, description, instructions, category_id) VALUES (%s, %s, %s, %s)"
-        values = (name, description, instructions, category_id)
-        self.cursor.execute(query, values)
-        self.conn.commit()
+        try:
+            insert_recipe_query = "INSERT INTO recipes (name, description, instructions, category_id) VALUES (%s, %s, %s, %s)"
+            recipe_values = (name, description, instructions, category_id)
+            self.cursor.execute(insert_recipe_query, recipe_values)
+            self.connection.commit()
+            recipe_id = self.cursor.lastrowid
 
-    def get_recipe_by_id(self, recipe_id):
-        query = "SELECT * FROM recipes WHERE id = %s"
-        values = (recipe_id,)
-        self.cursor.execute(query, values)
-        recipe = self.cursor.fetchone()
-        if recipe:
-            recipe_id, name, description, instructions, category_id = recipe
-            category = self.get_category_by_id(category_id)
-            recipe_ingredients = self.get_recipe_ingredients(recipe_id)
-            ingredients = []
-            for recipe_ingredient in recipe_ingredients:
-                ingredient_id, quantity = recipe_ingredient[1:]
-                ingredient = self.get_ingredient_by_id(ingredient_id)
-                ingredients.append((ingredient, quantity))
-            return {
-                "id": recipe_id,
-                "name": name,
-                "description": description,
-                "instructions": instructions,
-                "category": category,
-                "ingredients": ingredients
-            }
-        else:
-            return None
-
-    def get_all_recipes(self):
-        query = "SELECT * FROM recipes"
-        self.cursor.execute(query)
-        recipes = self.cursor.fetchall()
-        all_recipes = []
-        for recipe in recipes:
-            recipe_id, name, description, instructions, category_id = recipe
-            category = self.get_category_by_id(category_id)
-            recipe_ingredients = self.get_recipe_ingredients(recipe_id)
-            ingredients = []
-            for recipe_ingredient in recipe_ingredients:
-                ingredient_id, quantity = recipe_ingredient[1:]
-                ingredient = self.get_ingredient_by_id(ingredient_id)
-                ingredients.append((ingredient, quantity))
-            all_recipes.append({
-                "id": recipe_id,
-                "name": name,
-                "description": description,
-                "instructions": instructions,
-                "category": category,
-                "ingredients": ingredients
-            })
-        return all_recipes
+            return recipe_id
+        except mysql.connector.Error as error:
+            print("Failed to insert record into recipes table: {}".format(error))
 
     def add_ingredient(self, name, unit):
-        query = "INSERT INTO ingredients (name, unit) VALUES (%s, %s)"
-        values = (name, unit)
-        self.cursor.execute(query, values)
-        self.conn.commit()
+        try:
+            insert_ingredient_query = "INSERT INTO ingredients (name, unit) VALUES (%s, %s)"
+            ingredient_values = (name, unit)
+            self.cursor.execute(insert_ingredient_query, ingredient_values)
+            self.connection.commit()
+            ingredient_id = self.cursor.lastrowid
 
-    def get_ingredient_by_id(self, ingredient_id):
-        query = "SELECT * FROM ingredients WHERE id = %s"
-        values = (ingredient_id,)
-        self.cursor.execute(query, values)
-        ingredient = self.cursor.fetchone()
-        if ingredient:
-            ingredient_id, name, unit = ingredient
-            return {
-                "id": ingredient_id,
-                "name": name,
-                "unit": unit
-            }
-        else:
-            return None
-
-    def get_all_ingredients(self):
-            query = "SELECT * FROM ingredients"
-            self.cursor.execute(query)
-            ingredients = self.cursor.fetchall()
-            all_ingredients = []
-            for ingredient in ingredients:
-                ingredient_id, name, unit = ingredient
-                all_ingredients.append({
-                    "id": ingredient_id,
-                    "name": name,
-                    "unit": unit
-                })
-            return all_ingredients
+            return ingredient_id
+        except mysql.connector.Error as error:
+            print("Failed to insert record into ingredients table: {}".format(error))
 
     def add_recipe_ingredient(self, recipe_id, ingredient_id, quantity):
-        query = "INSERT INTO recipe_ingredients (recipe_id, ingredient_id, quantity) VALUES (%s, %s, %s)"
-        values = (recipe_id, ingredient_id, quantity)
-        self.cursor.execute(query, values)
-        self.conn.commit()
-
-    def get_recipe_ingredients(self, recipe_id):
-        query = "SELECT * FROM recipe_ingredients WHERE recipe_id = %s"
-        values = (recipe_id,)
-        self.cursor.execute(query, values)
-        recipe_ingredients = self.cursor.fetchall()
-        return recipe_ingredients
+        try:
+            insert_recipe_ingredient_query = "INSERT INTO recipe_ingredients (recipe_id, ingredient_id, quantity) VALUES (%s, %s, %s)"
+            recipe_ingredient_values = (recipe_id, ingredient_id, quantity)
+            self.cursor.execute(insert_recipe_ingredient_query, recipe_ingredient_values)
+            self.connection.commit()
+        except mysql.connector.Error as error:
+            print("Failed to insert record into recipe_ingredients table: {}".format(error))
 
     def add_category(self, name):
-        query = "INSERT INTO categories (name) VALUES (%s)"
-        values = (name,)
-        self.cursor.execute(query, values)
-        self.conn.commit()
+        try:
+            insert_category_query = "INSERT INTO categories (name) VALUES (%s)"
+            category_values = (name,)
+            self.cursor.execute(insert_category_query, category_values)
+            self.connection.commit()
+            category_id = self.cursor.lastrowid
 
-    def get_category_by_id(self, category_id):
-        query = "SELECT * FROM categories WHERE id = %s"
-        values = (category_id,)
-        self.cursor.execute(query, values)
-        category = self.cursor.fetchone()
-        if category:
-            category_id, name = category
-            return {
-                "id": category_id,
-                "name": name
-            }
-        else:
+            return category_id
+        except mysql.connector.Error as error:
+            print("Failed to insert record into categories table: {}".format(error))
+
+    def get_recipe(self, recipe_id):
+        try:
+            select_recipe_query = "SELECT * FROM recipes WHERE id=%s"
+            self.cursor.execute(select_recipe_query, (recipe_id,))
+            recipe = self.cursor.fetchone()
+            if recipe:
+                return recipe
+            else:
+                print("No recipe found with id = {}".format(recipe_id))
+        except mysql.connector.Error as error:
+            print("Failed to retrieve record from recipes table: {}".format(error))
+
+    def get_ingredient(self, ingredient_id):
+        try:
+            select_ingredient_query = "SELECT * FROM ingredients WHERE id=%s"
+            self.cursor.execute(select_ingredient_query, (ingredient_id,))
+            ingredient = self.cursor.fetchone()
+            if ingredient:
+                return ingredient
+            else:
+                print("No ingredient found with id = {}".format(ingredient_id))
+        except mysql.connector.Error as error:
+            print("Failed to retrieve record from ingredients table: {}".format(error))
+            
+    def get_category(self, category_id):
+        try:
+            pass
+        
+        except Exception as e:
+            print(f"Error getting category with id {category_id}: {e}")
             return None
-
-    def get_all_categories(self):
-        query = "SELECT * FROM categories"
-        self.cursor.execute(query)
-        categories = self.cursor.fetchall()
-        all_categories = []
-        for category in categories:
-            category_id, name = category
-            all_categories.append({
-                "id": category_id,
-                "name": name
-            })
-        return all_categories
         
         
     def __init__(self) -> None:
