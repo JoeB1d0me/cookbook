62e69347491c816092749d3ade53e5289ded3ac3
Kingsley Ulinfun <kingsleyulinfun@gmail.com>
2023-04-30 16:13:47 -0400

    Stuff

--- gui/app.py
+++ gui/app.py
@@ -2,7 +2,7 @@
 CS50AP
 Master Project
 """
-from database.struct import DataStructure
+from struct import DataStructure
 import tkinter as tk
 from tkinter import *
 from tkinter import Menu
@@ -47,10 +47,76 @@ class BookApp():
         
         # Menu bar
         self.menubar = tk.Menu(self.root)
-        self.root.config(menu=menubar)
-        self.file_menu = tk.Menu(menubar)
-        self.menubar.add_cascade(label="File", menu=file_menu)
-        self.add_command(label="Exit", command=self.exit_program)
-        help_menu = tk.Menu(menubar)
-        menubar.add_cascade(label="Help", menu=help_menu)
-        help_menu.add_command(label="About", command=self.about)
+        self.root.config(menu=self.menubar)
+        self.file_menu = tk.Menu(self.menubar)
+        self.menubar.add_cascade(label="File", menu=self.file_menu)
+        self.file_menu.add_command(label="Exit", command=self.exit)
+        self.help_menu = tk.Menu(self.menubar)
+        self.menubar.add_cascade(label="Help", menu=self.help_menu)
+        self.help_menu.add_command(label="About", command=self.about)
+        
+        self.buttonsFrame = tk.Frame(self.mainFrame)
+        self.buttonsFrame.grid(row=0, column=0, padx=10, pady=10, sticky="n")
+        
+        self.newButton = tk.Button(self.buttonsFrame, text="New", width=10)
+        self.newButton.grid(row=0, column=0, padx=5, pady=5)
+        self.editButton = tk.Button(self.buttonsFrame, text="Edit", width=10)
+        self.editButton.grid(row=0, column=1, padx=5, pady=5)
+        self.deleteButton = tk.Button(self.buttonsFrame, text="Delete", width=10)
+        self.deleteButton.grid(row=0, column=2, padx=5, pady=5)
+        
+        # create data entry frame
+        self.dataEntryFrame = tk.Frame(self.mainFrame)
+        self.dataEntryFrame.grid(row=1, column=0, padx=10, pady=10, sticky="w")
+        
+        self.recipeLabel = tk.Label(self.dataEntryFrame, text="Recipe:")
+        self.recipeLabel.grid(row=0, column=0, padx=5, pady=5, sticky="w")
+        self.recipeEntry = tk.Entry(self.dataEntryFrame, width=30)
+        self.recipeEntry.grid(row=0, column=1, padx=5, pady=5)
+        
+        self.categoryLabel = tk.Label(self.dataEntryFrame, text="Category:")
+        self.categoryLabel.grid(row=1, column=0, padx=5, pady=5, sticky="w")
+        self.categoryOption = tk.OptionMenu(self.dataEntryFrame, tk.StringVar(), "Select Category", "Category 1", "Category 2", "Category 3")
+        self.categoryOption.grid(row=1, column=1, padx=5, pady=5)
+        
+        self.ingredientFrame = tk.Frame(self.mainFrame)
+        self.ingredientFrame.grid(row=3, column=0, sticky="nsew", padx=10, pady=10)
+
+# create labels and option menus for ingredient data
+        self.ingredient_label = tk.Label(self.ingredientFrame, text="Ingredients", font=("Helvetica", 16))
+        self.ingredient_label.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
+
+        self.type_label = tk.Label(self.ingredientFrame, text="Type", font=("Helvetica", 12))
+        self.type_label.grid(row=1, column=0, padx=10, pady=10, sticky="w")
+
+        types = ["Select Type", "Produce", "Protein", "Grain", "Dairy", "Other"]
+        self.type_var = tk.StringVar(self.ingredientFrame)
+        self.type_var.set(types[0])
+        self.type_option = tk.OptionMenu(self.ingredientFrame, self.type_var, *types)
+        self.type_option.grid(row=1, column=1, padx=10, pady=10)
+
+        self.name_label = tk.Label(self.ingredientFrame, text="Name", font=("Helvetica", 12))
+        self.name_label.grid(row=2, column=0, padx=10, pady=10, sticky="w")
+        self.name_entry = tk.Entry(self.ingredientFrame, width=30)
+        self.name_entry.grid(row=2, column=1, padx=10, pady=10)
+
+        self.quantity_label = tk.Label(self.ingredientFrame, text="Quantity", font=("Helvetica", 12))
+        self.self.quantity_label.grid(row=3, column=0, padx=10, pady=10, sticky="w")
+        self.quantity_entry = tk.Entry(self.ingredientFrame, width=10)
+        self.quantity_entry.grid(row=3, column=1, padx=10, pady=10)
+
+        self.unit_label = tk.Label(self.ingredientFrame, text="Unit", font=("Helvetica", 12))
+        self.unit_label.grid(row=3, column=2, padx=10, pady=10, sticky="w")
+        units = ["Select Unit", "tsp", "tbsp", "cup", "oz", "lb", "kg"]
+        self.unit_var = tk.StringVar(self.ingredientFrame)
+        self.unit_var.set(units[0])
+        self.unit_option = tk.OptionMenu(self.ingredientFrame, self.unit_var, *units)
+        self.unit_option.grid(row=3, column=3, padx=10, pady=10)
+
+        self.add_ingredient_btn = tk.Button(self.ingredientFrame, text="Add", font=("Helvetica", 12))
+        self.add_ingredient_btn.grid(row=4, column=3, padx=10, pady=10, sticky="e")
+        self.remove_ingredient_btn = tk.Button(self.ingredientFrame, text="Remove", font=("Helvetica", 12))
+        self.remove_ingredient_btn.grid(row=4, column=2, padx=10, pady=10, sticky="e")
+                
+        
+        self.root.mainloop()
---
+++ gui/main.py
@@ -0,0 +1,11 @@
+"""Kingsley Ulinfun
+CS50 AP
+Master Project- Cookbook/Recipebook
+main.py"""
+
+from app import BookApp
+def main():
+    app = BookApp()
+    
+if(__name__ == "__main__"):
+    main()
